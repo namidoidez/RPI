a0a19335aa6583fd6fb7fa3fb72f805d4e4807d0
namidoidez <158545440+namidoidez@users.noreply.github.com>
2024-03-15 09:29:45 +0300

    add 2nd semestr

---
+++ 2 семестр/ЛР4/main/index.js
@@ -0,0 +1,12 @@
+let searchButton = document.getElementById("searchButton");
+let album = document.getElementById(booksList);
+
+searchButton.addEventListener("click", () => {
+	let input = document.getElementById(`searchInput`);
+	let value = input.value.toLowerCase();
+
+	album.filter(
+		function () {
+
+	});
+});
