26a4661cf605cd5fe6237ef75adaf40d316d1e7f
namidoidez <158545440+namidoidez@users.noreply.github.com>
2024-05-20 18:50:33 +0300

    Add LR11

---
+++ 2 семестр/ЛР11/3/admin/admin.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+
+
+admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+def login_admin():
+    session['admin_logged'] = 1
+
+def isLogged():
+    return True if session.get('admin_logged') else False
+
+def logout_admin():
+    session.pop('admin_logged', None)
+
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "admin":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+    return render_template('admin/login.html', title='Админ-панель')
+
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    logout_admin()
+
+    return redirect(url_for('.login'))
